16b60924eedc9c8056f5479ad666766bc4369891
Riya agrawal <riyaagrawal2712@gmail.com>
2025-10-17 20:49:12 +0530

    Add files via upload

---
+++ listOf3FavMovie.py
@@ -0,0 +1,7 @@
+#WAP to ask the user to enter names of their 3 favorite movies & store them in a list.
+a=input("enter first favourite movie: ") #bydefault its an string
+b=input("enter second favourite movie: ")
+c=input("enter third favourite movie: ")
+fav_movie=[a,b,c]
+print(fav_movie)
+print(type(fav_movie))
