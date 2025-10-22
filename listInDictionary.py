74f41908d2d5ea3212438b4777ef6dc506553d69
Riya agrawal <riyaagrawal2712@gmail.com>
2025-10-22 13:36:46 +0530

    Add files via upload

---
+++ listInDictionary.py
@@ -0,0 +1,8 @@
+#Store following word meanings in a python dictionary :
+#table : “a piece of furniture”,“list of facts & figures”
+#cat : “a small animal”
+dic={
+    "table":["a piece of furniture", "list of fact & figures"],
+    "cat":"a small animal"
+}
+print(dic)
