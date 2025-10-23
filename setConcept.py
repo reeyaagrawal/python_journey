99a40f202adade7acf90ebf1b7edfca2c4133557
Riya agrawal <riyaagrawal2712@gmail.com>
2025-10-23 16:36:51 +0530

    Add files via upload

---
+++ setConcept.py
@@ -0,0 +1,6 @@
+#You are given a list of subjects for students. Assume one classroom is required for 1subject. How many classrooms are needed by all students.
+#{”python”,“java”,“C++”,“python”,“javascript”,“java”,“python”,“java”,“C++”,“C”}
+#now we discuss set
+subject=['python','java','c++','python','javascript','java','python','java','c++','c']
+print(set(subject))
+print(len(set(subject)))
